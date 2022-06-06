4f2f40e9a5999a99af00719245668470035bdd06
dandon1990 <daniel.don@icloud.com>
2022-06-06 21:15:34 +0000

    Add main function back

--- run.py
+++ run.py
@@ -131,7 +131,6 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-
+    sales_columns = get_last_5_entries_sales()
 print("Welcome to Love Sandwiches Data Automation")
-# main()
-sales_columns = get_last_5_entries_sales()
+main()
